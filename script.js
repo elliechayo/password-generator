7d5c76b11d01b1a71cc02dd7b46518021b14d3fe
Elinor Chayo <elliechayo@gmail.com>
2023-02-13 22:48:50 -0500

    modified js file to prevent html error

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function generatePassword() {
+
+  var length = Number(prompt("Enter the length of the password"));
+
+  var password = "";
+  var passwordChars = "";
+
+  // length validation
+  if (!length || length < 8 || length > 128) {
+    alert("Invalid length. Length must be between the range 8 and 128");
+    return "";
+  };
+
+  //character validation
+  var characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    special: "!@#$%^&*()",
+  }
+
+  
+  
+
+
+
+  // add criteria to select
+  if (confirm("Click OK to include lowercase letters?")) {
+    passwordChars += characters.lowercase;
+  }
+  if (confirm("Click OK to include uppercase letters?")) {
+    passwordChars += characters.uppercase;
+  }
+  if (confirm("Click OK to include numbers?")) {
+    passwordChars += characters.numbers;
+  }
+  if (confirm("Click OK to include Symbols?")) {
+    passwordChars += characters.special;
+  }
+
+  var passCharLength = passwordChars.length;
+
+  // If no criteria is selected, return an empty
+  if (passCharLength <= 0) 
+  return "";
+
+  // loops `length` times and pick a random char from passwordChars
+  for (let i = 0; i < length; i++) {
+    var randInt = Math.floor(Math.random() * passCharLength);
+    password += passwordChars[randInt];
+  }
+  return password;
+}
+
+// password input
 function writePassword() {
   var password = generatePassword();
+  console.log (password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
